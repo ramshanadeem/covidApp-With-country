4ac6f9f933c93c25f35e9efe94e921308d56540f
ramshanadeem <rimshanadeem64@gmial.com>
2021-02-19 10:35:58 +0500

    Updates

---
+++ precache-manifest.210ea1bf27636de660816743ab6b6c0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a265f838826a97f526f6edc1cc49074a",
+    "url": "/covidapp-With-country/index.html"
+  },
+  {
+    "revision": "dac2d926916c9d0a2a2f",
+    "url": "/covidapp-With-country/static/css/main.68389b93.chunk.css"
+  },
+  {
+    "revision": "c682e60dc8254c157e17",
+    "url": "/covidapp-With-country/static/js/2.f0ee0f1d.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covidapp-With-country/static/js/2.f0ee0f1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dac2d926916c9d0a2a2f",
+    "url": "/covidapp-With-country/static/js/main.def0aeb7.chunk.js"
+  },
+  {
+    "revision": "959badaf8ac29441713c",
+    "url": "/covidapp-With-country/static/js/runtime-main.2a6bde50.js"
+  }
+]);
